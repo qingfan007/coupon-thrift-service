ebd94e52eba62aeac55cd48165560e8758880932
crystal_zhu <zhudandan_seu@163.com>
2025-06-22 19:49:54 +0800

    fix protocol

--- src/main/java/com/qingfan/thrift/server/ThriftCouponServer.java
+++ src/main/java/com/qingfan/thrift/server/ThriftCouponServer.java
@@ -1,8 +1,9 @@
 package com.qingfan.thrift.server;
 
 import com.qingfan.thrift.iface.CouponService;
+import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 
@@ -10,10 +11,19 @@ public class ThriftCouponServer {
 
     public static void main(String[] args) {
         try {
-            CouponService.Processor<CouponService.Iface> processor = new CouponService.Processor<>(new CouponServiceImpl());
             TServerTransport serverTransport = new TServerSocket(9090);
-            TServer server = new TSimpleServer(new TServer.Args(serverTransport).processor(processor));
-//            System.out.println("Coupon Thrift Server started on port 9090");
+            TBinaryProtocol.Factory protocolFactory = new TBinaryProtocol.Factory();
+
+            CouponService.Processor<CouponService.Iface> processor =
+                    new CouponService.Processor<>(new CouponServiceImpl());
+
+            TServer server = new TThreadPoolServer(
+                    new TThreadPoolServer.Args(serverTransport)
+                            .processor(processor)
+                            .protocolFactory(protocolFactory)
+            );
+
+            System.out.println("Coupon Thrift Server started on port 9090");
             server.serve();
         } catch (Exception e) {
             e.printStackTrace();
